c4238cdaf2bc6f20a252d60b21b9633f723a9e22
Angelo Dylan <angelo.quispe.l@tecsup.edu.pe>
2025-10-29 20:02:35 -0500

    Feat: Cambiar motor de base de datos a Postgresql

--- vot_core/settings.py
+++ vot_core/settings.py
@@ -92,12 +92,23 @@ WSGI_APPLICATION = 'vot_core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# Use PostgreSQL if DATABASE_URL is set, otherwise fallback to SQLite
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
